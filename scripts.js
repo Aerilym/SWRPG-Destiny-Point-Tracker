7de531fbb5141a3a173f2130a9f477dbcac1f4e3
Aerilym <aerilymbiz@gmail.com>
2021-07-12 22:21:26 +1000

    added darkmode and removed transparent background

--- scripts.js
+++ scripts.js
@@ -90,3 +90,49 @@ function togglehide(item, basedisplay) {
     x.style.display = "none";
     }
 }
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+function darkmodetoggle() {
+    var myCookie = getCookie("darkmodepref");
+    if (myCookie.includes('False')){
+      document.cookie = "darkmodepref=True; expires=Thu, 18 Dec 2030 12:00:00 UTC";
+    } else if (myCookie.includes('True')){
+      document.cookie = "darkmodepref=False; expires=Thu, 18 Dec 2030 12:00:00 UTC";
+    }
+    document.body.classList.toggle("darkmode");
+    }
+
+function checkdarkpref(){
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    var myCookie = getCookie("darkmodepref");
+    if (myCookie == null){
+        if (prefersDarkScheme.matches) {
+                document.cookie = "darkmodepref=True; expires=Thu, 18 Dec 2030 12:00:00 UTC";
+                document.body.classList.toggle("darkmode");
+        }
+    } else if (myCookie.includes('True')){
+        document.body.classList.toggle("darkmode");
+    } else {
+        document.cookie = "darkmodepref=False; expires=Thu, 18 Dec 2030 12:00:00 UTC";
+    }
+}
