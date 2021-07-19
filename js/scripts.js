e5792d321008a87e7ff6baeb86cc323a69633a38
Aerilym <aerilymbiz@gmail.com>
2021-07-19 18:55:43 +1000

    version checking

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // For front end website functions -- Created by Ryan Miller https://aerilym.github.io/
-let version = '1.0.1';
+let version = '1.1.0';
 
 
 function togglehide(item, basedisplay) {
@@ -163,7 +163,7 @@ function isoldversion(userversion) {
 }
 
 function newfeatures(userversion) {
-    firsttime()
+    alert('Update: You can now change the token images to presets and custom images.')
 }
 
 function firsttime() {
