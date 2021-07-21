7241a2c06ba26f6e7fa0a9858f57c2f558c1d7fd
Aerilym <aerilymbiz@gmail.com>
2021-07-21 11:54:01 +1000

    keybinds can now be mapped by the user

--- js/keybinds.js
+++ js/keybinds.js
@@ -3,67 +3,120 @@
 //NOTE pressing shift and = is actualy shift and + as well as just pressing +, as when you hold shift it changes the = key to +, this happens to all keys with a shift option.
 //NOTE the + symbol in the keybinds indicated a key combination, so shift+h is holding shift and clicking h
 //BINDS             1           2     Non-Shift
-hidebuttons =   ['shift+h',     '',     'h']        //Hide/Unhide buttons
-hideeye =       ['shift+e',     '',     'e']        //Hide/Unhide eye button
 adddestiny =    ['shift+=',     '+',    '=']        //Add destiny point
 removedestiny = ['shift+-',     '_',    '-']        //Remove destiny point
 fliptolight =   ['shift+up',    '',     'up']       //Flip dark side point to light
 fliptodark =    ['shift+down',  '',     'down']     //Flip light side point to dark
+hidebuttons =   ['shift+h',     '',     'h']        //Hide/Unhide buttons
+hideeye =       ['shift+e',     '',     'e']        //Hide/Unhide eye button
 
-binds = ['hidebuttons', 'hideeye', 'adddestiny', 'removedestiny', 'fliptolight', 'fliptodark']
-bindsmap = [hidebuttons, hideeye, adddestiny, removedestiny, fliptolight, fliptodark]
-
-//Hide/Unhide the buttons and information below the tool
-Mousetrap.bind(hidebuttons[0], function(e) {
-    togglehide('settings','block')
-    return false;
-});
+binds = ['adddestiny', 'removedestiny', 'fliptolight', 'fliptodark', 'hidebuttons', 'hideeye']
+bindsmap = [adddestiny, removedestiny, fliptolight, fliptodark, hidebuttons, hideeye]
 
 //Adds a destiny point to the tracker
-Mousetrap.bind(adddestiny[0], function(e) {
-    add_destiny()
-    return false;
-});
-
-//Adds a destiny point to the tracker
-Mousetrap.bind(adddestiny[1], function(e) {
-    add_destiny()
-    return false;
-});
+function bindadddestiny(keybinds) {
+    if (keybinds.length == 1) {
+        keybinds.push('')        
+    }
+    for (var i = 0; i < keybinds.length-1; i++) {
+        if (keybinds[i] != '') {
+            Mousetrap.bind(keybinds[i], function(e) {
+                add_destiny()
+                return false;
+            });
+        }
+    }
+}
 
 //Removes a destiny point from the tracker
-Mousetrap.bind(removedestiny[0], function(e) {
-    remove_destiny()
-    return false;
-});
-
-//Removes a desitny point from the tracker
-Mousetrap.bind(removedestiny[1], function(e) {
-    remove_destiny()
-    return false;
-});
-
-//Hide/Unhide the eye icon (the icon used for hiding the buttons)
-Mousetrap.bind(hideeye[0], function(e) {
-    deleteeye('key')
-    return false;
-});
+function bindremovedestiny(keybinds) {
+    if (keybinds.length == 1) {
+        keybinds.push('')        
+    }
+    for (var i = 0; i < keybinds.length-1; i++) {
+        if (keybinds[i] != '') {
+            Mousetrap.bind(keybinds[i], function(e) {
+                remove_destiny()
+                return false;
+            });
+        }
+    }
+}
 
 //Flips a dark side destiny point to a light side destiny point
-Mousetrap.bind(fliptolight[0], function(e) {
-    flip('light')
-    return false;
-});
+function bindfliptolight(keybinds) {
+    if (keybinds.length == 1) {
+        keybinds.push('')        
+    }
+    for (var i = 0; i < keybinds.length-1; i++) {
+        if (keybinds[i] != '') {
+            Mousetrap.bind(keybinds[i], function(e) {
+                flip('light')
+                return false;
+            });
+        }
+    }
+}
 
 //Flips a light side destiny point to a dark side destiny point
-Mousetrap.bind(fliptodark[0], function(e) {
-    flip('dark')
-    return false;
-});
+function bindfliptodark(keybinds) {
+    if (keybinds.length == 1) {
+        keybinds.push('')        
+    }
+    for (var i = 0; i < keybinds.length-1; i++) {
+        if (keybinds[i] != '') {
+            Mousetrap.bind(keybinds[i], function(e) {
+                flip('dark')
+                return false;
+            });
+        }
+    }
+}
+
+
+//Hide/Unhide the buttons and information below the tool
+function bindhidebuttons(keybinds) {
+    if (keybinds.length == 1) {
+        keybinds.push('')        
+    }
+    for (var i = 0; i < keybinds.length-1; i++) {
+        if (keybinds[i] != '') {
+            Mousetrap.bind(keybinds[i], function(e) {
+                togglehide('settings','block')
+                return false;
+            });
+        }
+    }
+}
+
+//Hide/Unhide the eye icon (the icon used for hiding the buttons)
+function bindhideeye(keybinds) {
+    if (keybinds.length == 1) {
+        keybinds.push('')        
+    }
+    for (var i = 0; i < keybinds.length-1; i++) {
+        if (keybinds[i] != '') {
+            Mousetrap.bind(keybinds[i], function(e) {
+                deleteeye('key')
+                return false;
+            });
+        }
+    }
+}
 
 Mousetrap.bind('up up down down left right left right b a enter', function() {
     alert('nice')
 });
+
+function setkeybinds(keybindsmap) {
+    bindadddestiny(keybindsmap[0])
+    bindremovedestiny(keybindsmap[1])
+    bindfliptolight(keybindsmap[2])
+    bindfliptodark(keybindsmap[3])
+    bindhidebuttons(keybindsmap[4])
+    bindhideeye(keybindsmap[5])
+}
+setkeybinds(bindsmap)
 
 //Non-shift keybinds (only active when the button on the page is active)
 function bindnonshift() {
@@ -105,7 +158,7 @@ function bindnonshift() {
     });
 }
 
-//Umbinds Non-shift keybinds when called
+//Unbinds Non-shift keybinds when called
 function unbindnonshift() {
     Mousetrap.unbind(adddestiny[2]);
     Mousetrap.unbind(removedestiny[2]);
@@ -115,6 +168,15 @@ function unbindnonshift() {
     Mousetrap.unbind(fliptodark[2]);
 }
 
+//Umbinds Non-shift keybinds when called
+function unbindall() {
+    for (var i = 0; i < bindsmap.length; i++) {
+        for (var j = 0; j < adddestiny.length-1; j++) {
+            Mousetrap.unbind(bindsmap[i][j])
+        }
+    }
+}
+
 function writekeybinds() {
     var keynum = adddestiny.length
     var numbinds = binds.length
@@ -122,7 +184,30 @@ function writekeybinds() {
         for (var j = 0; j < keynum; j++) {
             docbind = document.getElementById('KB' + binds[i] + j)
             docbind.innerText = bindsmap[i][j].replace('+',' + ')
-            // tooltip.onclick = function(){Mousetrap.unbind(docbind.innerText = binds[i].replace(' + ','+'));};
+            docbind.onclick = function(){changebind(this.id);};
         }
     }
+}
+
+function changebind(elementID) {
+    var bindelement = document.getElementById(elementID)
+    var oldBind = bindelement.innerText.replace(' + ','+')
+    console.log(oldBind)
+    Mousetrap.unbind(oldBind)
+    result = window.prompt('Change keybind', oldBind);
+    var fnstr = elementID.slice(2,-1)
+    if (fnstr == 'adddestiny') {
+        bindadddestiny([result])
+    } else if (fnstr == 'removedestiny') {
+        bindremovedestiny([result])
+    } else if (fnstr == 'fliptolight') {
+        bindfliptolight([result])
+    } else if (fnstr == 'fliptodark') {
+        bindfliptodark([result])
+    } else if (fnstr == 'hidebuttons') {
+        bindhidebuttons([result])
+    } else if (fnstr == 'hideeye') {
+        bindhideeye([result])
+    }
+    bindelement.innerText = result
 }
